d9077f68ba83f0c2d87c973caa844ed45e0f294c
Li Yuchen <sprball@icloud.com>
2025-12-11 13:21:00 +0800

    Rename BracketBalanceChecker to q2

--- viva2/src/q2.java
+++ viva2/src/q2.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Stack; 
 
-public class BracketBalanceChecker {
+public class q2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter an Expression: ");
